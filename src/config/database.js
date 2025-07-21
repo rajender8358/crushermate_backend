936e11c194a3bb89e75d43723cbb051487a51077
rajender8358 <rajenderreddy.garlapalli@gmail.com>
2025-07-22 00:06:24 +0530

    Fix: Remove deprecated bufferMaxEntries option from MongoDB connection

--- src/config/database.js
+++ src/config/database.js
@@ -14,7 +14,6 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
       bufferCommands: false, // Disable mongoose buffering
     });
 
